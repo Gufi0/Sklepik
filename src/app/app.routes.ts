7d5a3d8f70e0fd693ddf4f6bbbf294badbfc925d
Gufi <i.wolczak@uczen.eduwarszawa.pl>
2025-04-15 12:54:54 +0200

    s

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'products', loadComponent: () => import('./products/product-list/product-list.component').then(m => m.ProductListComponent)},
+];
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,33 +1,37 @@
 // src/app/products/product-list/product-list.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForOf } from '@angular/common';
 import { ProductComponent } from '../product/product.component';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [ProductComponent, NgForOf],
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
-  products = [
-    { name: 'Bread', price: 15,image:'public/zdj.jpg' },
-    { name: 'Butter', price: 20,image:'public/zdj.jpg' },
-    { name: 'Sausages', price: 35,image:'public/zdj.jpg' },
-    { name: 'Eggs', price: 10, image:'public/zdj.jpg' },
-    { name: 'Pork', price: 150, image:'public/zdj.jpg' },
-    { name: 'Mutton', price: 200, image:'public/zdj.jpg' },
-    { name: 'Beef', price: 175,image:'public/zdj.jpg' },
-    { name: 'Sushi', price: 500, image:'public/zdj.jpg' },
-    { name: 'Pizza', price: 60, image:'public/zdj.jpg' },
-    { name: 'Water', price: 5, image:'public/zdj.jpg' },
-    { name: 'Redbull', price: 11, image:'public/zdj.jpg' },
-    { name: 'Beer', price: 25, image:'public/zdj.jpg' },
-    { name: 'Vodka', price: 50, image:'public/zdj.jpg' },
-    { name: 'Whisky', price: 140, image:'public/zdj.jpg' },
-  ];
+export class ProductListComponent implements OnInit {
+  products: any[] = [];
 
-  onBuy(productName: string) {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  fetchProducts(): void {
+    this.http.get<any[]>('http://localhost:3000/products').subscribe(
+      (data) => {
+        this.products = data;
+      },
+      (error) => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
+
+  onBuy(productName: string): void {
     console.log(`${productName} was bought!`);
   }
 }
